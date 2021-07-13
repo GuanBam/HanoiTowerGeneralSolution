bbfe5b1b54521000b6db6c1d2d7a972a5b3931a3
GuanBam <37057534+GuanBam@users.noreply.github.com>
2021-07-12 19:16:21 -0500

    Add files via upload

---
+++ GeneralSolution_Java/Main.java
@@ -0,0 +1,18 @@
+/**
+ * Main Class
+ * @author tianyang guan tyguancn@gmail.com
+ */
+class Main {
+    /**
+     * Main class
+     * @param args args
+     */
+    public static void main(String[] args){
+        /* Class used to obtain user input for number of Tower and Disk */
+        HanoiTowerInput HanoiTower = new HanoiTowerInput();
+        /* Request input for number of Tower and Disk */
+        HanoiTower.RequestInput();
+        /* Start to find solution */
+        HanoiTower.StartSolution();
+    }
+}
